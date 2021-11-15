6f7ea42928819d287738cbb7b858a4143bb7534c
Krllka <k.r.llaleks25@gmail.com>
2021-11-15 21:53:12 +0400

    add common controller for baseURl

---
+++ src/main/java/com/backend/backend/controllers/impl/CommonController.java
@@ -0,0 +1,10 @@
+package com.backend.backend.controllers.impl;
+
+import com.backend.backend.controllers.RESTController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CommonController extends RESTController {
+}
